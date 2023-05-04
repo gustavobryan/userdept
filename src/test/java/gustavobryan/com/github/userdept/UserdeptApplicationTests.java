96a8daa97620d47d8b0a0771363f30b08fe6ffaf
Gustavo Bryan <devgustavob@gmail.com>
2023-05-03 21:21:19 -0300

    commit inicial

---
+++ src/test/java/gustavobryan/com/github/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavobryan.com.github.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
